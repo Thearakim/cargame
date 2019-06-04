6ed32ee4212713a69bbac7e034d360f105e3a629
Theara Kim <45727527+Thearakim@users.noreply.github.com>
2019-06-04 14:10:18 +0700

    Update cargame.py

--- cargame.py
+++ cargame.py
@@ -1,14 +1,25 @@
 command = ""
-while command != "quit":
+started = False
+while True:
     command = input(">>").lower()
     if command == "start":
-        print("Start the car.")
+        if started:
+            print("The car have already started!!")
+        else:
+            started = True
+            print("Start the car.")
     elif command == "stop":
-        print("Stop the car.")
+        if not started:
+            print("Car is already stopped!!!")
+        else:
+            started = False
+            print("Stop the car.")
     elif command == "help":
         print("""
                 Start-to start the car
                 stop-to stop the car
                 quit-to quit the game""")
+    elif command =="quit":
+        break
     else:
         print("Sorry i don't understand that")
